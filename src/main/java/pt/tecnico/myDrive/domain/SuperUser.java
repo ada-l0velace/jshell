7a166bf2f35a6580b7a6550ad9ea4e46a88e0fcb
qwelas <qwelas007@gmail.com>
2016-03-08 23:51:29 +0000

    Added Class SuperUser
    :

--- src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
+++ src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
@@ -1,9 +1,29 @@
 package pt.tecnico.myDrive.domain;
+import org.joda.time.DateTime;
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.User;
 
+
+
+/**
+ * Identifies the admin that as total access to all files.
+ */
 public class SuperUser extends SuperUser_Base {
-    
+	/*Defines the properaties of the SuperUser
+	 * its username, name, directory and mask
+	 * */
+	
+	public static String ROOT_USERNAME = "root";
+	public static String ROOT_NAME = "Super User";
+	public static String ROOT_DIRECTORY = "/home/root";
+	public static Short ROOT_UMASK = (short) Integer.parseInt("F2",16);
+	/**
+	 * Default Constructor
+	 */
+	
 	public SuperUser() {
 		super();
+		super.init(ROOT_NAME,ROOT_USERNAME,"***",(short) ROOT_UMASK);
 	}
     
 }
