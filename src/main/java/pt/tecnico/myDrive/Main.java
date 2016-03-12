46d44d72378f427765e18d7d0896cc44bfaa20f6
nucleardannyd <daniel.leitao@ist.utl.pt>
2016-03-12 03:21:57 +0000

    created xmlprint method in the main class

--- src/main/java/pt/tecnico/myDrive/Main.java
+++ src/main/java/pt/tecnico/myDrive/Main.java
@@ -21,75 +21,82 @@ import java.io.IOException;
 
 public class Main {
 
-    private static final Logger logger = LogManager.getRootLogger();
+  private static final Logger logger = LogManager.getRootLogger();
     
-    public static void main(String [] args) {
-        try {
-            //logger.trace("Entering application.");
-            //for (String s: args) xmlScan(new java.io.File(s));
-            applicationCodeGoesHere();
-        } finally {
-            // ensure an orderly shutdown
-            FenixFramework.shutdown();
-        }
+  public static void main(String [] args) {
+    try {
+      //logger.trace("Entering application.");
+      //for (String s: args) xmlScan(new java.io.File(s));
+      applicationCodeGoesHere();
+    } finally {
+      // ensure an orderly shutdown
+      FenixFramework.shutdown();
     }
+  }
 
-    public static void applicationCodeGoesHere() {
-        someTransaction();
-    }
+  public static void applicationCodeGoesHere() {
+    someTransaction();
+  }
 
-    @Atomic
-    public static void someTransaction() {
-        /*
-        logger.trace(m.getHome().getName());
-        for (File f : m.getHome().getFileSet() ) {
-            if (f.getName().equals("home")) {
-                logger.trace(f.getName());
-                Directory a = (Directory) f;
-                for (File f1 : a.getFileSet())
-                    logger.trace(f1.getName());
-            }
+  @Atomic
+  public static void someTransaction() {
+    /*
+      logger.trace(m.getHome().getName());
+      for (File f : m.getHome().getFileSet() ) {
+      if (f.getName().equals("home")) {
+      logger.trace(f.getName());
+      Directory a = (Directory) f;
+      for (File f1 : a.getFileSet())
+      logger.trace(f1.getName());
+      }
 
-        }
-        */
-        //logger.trace(m.getDirHome().getName());
-        Element element = new Element("mydrive");
-        Manager m = Manager.getInstance();
-        User su = m.getSuperuser();
-        File f = new File(su, "myFile", m);
-        PlainFile pf = new PlainFile(su, "poodlePlainFile", m);
-        App a = new App(su, "poodleApp", "Scripts bue bons", m);
-        Link l = new Link("poodleLink", f, "/home/root");
-        //User u1 = new User("Daniel", "poodle", "poodlePass", (short) 1);
+      }
+    */
+    //logger.trace(m.getDirHome().getName());
+    Element element = new Element("mydrive");
+    Manager m = Manager.getInstance();
+    User su = m.getSuperuser();
+    File f = new File(su, "myFile", m);
+    PlainFile pf = new PlainFile(su, "poodlePlainFile", m);
+    App a = new App(su, "poodleApp", "Scripts bue bons", m);
+    Link l = new Link("poodleLink", f, "/home/root/poodleLink");
+    //User u1 = new User("Daniel", "poodle", "poodlePass", (short) 1);
 
-
-        su.getHome().addFile(f);
-        //su.getHome().addFile(pf);
-        su.getHome().addFile(l);
-        m.createUser(su);
+    su.getHome().addFile(f);
+    //su.getHome().addFile(pf);
+    su.getHome().addFile(l);
+    m.createUser(su);
         
-        element.addContent(su.exportXml());
-        
-        Document doc = m.exportXml();
-        XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
-        try { xmlOutput.output(doc, new PrintStream(System.out));
-        } catch (IOException e) { System.out.println(e); }
-        //System.out.println("FenixFramework's root object is: " + FenixFramework.getDomainRoot());		
-    }
+    element.addContent(su.exportXml());
+  }
 
-    /**
-     * Scans the xml file and imports to database.
-     * @param file (Java.io.File) represents a file. 
-     */
-    @Atomic
-    public static void xmlScan(java.io.File file) {
-        Manager m = Manager.getInstance();
-        SAXBuilder builder = new SAXBuilder();
-        try {
-            Document document = (Document)builder.build(file);
-            m.importXml(document.getRootElement());
-        } catch (JDOMException | IOException e) {
-            e.printStackTrace();
-        }
+  /**
+   * Exports the Manager to a xml file
+   */
+  @Atomic
+  public static void xmlPrint() {
+    log.trace("xmlPrint: " + FenixFramework.getDomainRoot());
+    Document doc = Manager.getInstance().exportXml();
+    XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
+
+
+    try { xmlOutput.output(doc, new PrintStream(System.out));
+    } catch (IOException e) { System.out.println(e); }
+  }
+
+  /**
+   * Scans the xml file and imports to database.
+   * @param file (Java.io.File) represents a file. 
+   */
+  @Atomic
+  public static void xmlScan(java.io.File file) {
+    Manager m = Manager.getInstance();
+    SAXBuilder builder = new SAXBuilder();
+    try {
+      Document document = (Document)builder.build(file);
+      m.importXml(document.getRootElement());
+    } catch (JDOMException | IOException e) {
+      e.printStackTrace();
     }
+  }
 }
