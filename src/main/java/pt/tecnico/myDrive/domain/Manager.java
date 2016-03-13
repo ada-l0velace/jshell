0f77b44f13fbe6af1541ea55e04811c133626c7c
nucleardannyd <daniel.leitao@ist.utl.pt>
2016-03-13 23:47:59 +0000

    removed rootdir from exportXml

--- src/main/java/pt/tecnico/myDrive/domain/Manager.java
+++ src/main/java/pt/tecnico/myDrive/domain/Manager.java
@@ -151,8 +151,6 @@ public class Manager extends Manager_Base{
     public Document exportXml() {
         Element node = new Element("Manager");
         Document doc = new Document(node);
-
-        node.addContent(getHome().exportXml());
         
         Element users = new Element("Users");
         node.addContent(users);
