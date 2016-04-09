945e3819188764859e143360c785a2d0881abe28
inestormenta <ist177980@tecnico.ulisboa.pt>
2016-04-09 16:37:38 +0100

    Added service base #40

---
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectory.java
@@ -0,0 +1,16 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+
+public class ListDirectory extends MyDriveService {
+
+    public ListDirectory(String token) {
+
+    }
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+
+    }
+}
