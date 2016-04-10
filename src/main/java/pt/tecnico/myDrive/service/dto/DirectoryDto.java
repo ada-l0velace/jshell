5105bf26cf619ce0d17c8f4a9d54ef0746d4e8ef
inestormenta <ist177980@tecnico.ulisboa.pt>
2016-04-10 22:55:02 +0100

    Added DirectoryDto. close #54

---
+++ src/main/java/pt/tecnico/myDrive/service/dto/DirectoryDto.java
@@ -0,0 +1,33 @@
+package pt.tecnico.myDrive.service.dto;
+
+import org.joda.time.DateTime;
+
+public class DirectoryDto extends FileDto {
+   
+    /**
+     * Construtor to DirectoryDto
+     * @param id (int) receives the id of the file.
+     * @param name (String) receives the name of the file.
+     * @param modified (DateTime) receives the date of the file.
+     * @param umask (Short) receives the permission mask of the file.
+     * @param parent (String) receives the name of the parent directory.
+     * @param owner (String) receives the owner name of the file.
+     */
+    public DirectoryDto(int id,
+                   String name,
+                   DateTime modified,
+                   short umask,
+                   String parent,
+                   String owner) {
+        super(id, name, modified, umask, parent, owner);  
+    }
+
+       /**
+     * Override of operator equal to a specific one.
+     * @param o represents the other object.
+     * @return boolean returns 1 if the name is equal to the other.
+     */
+    public int compareTo(DirectoryDto o) {
+        return getName().compareTo(o.getName());
+    }
+}
