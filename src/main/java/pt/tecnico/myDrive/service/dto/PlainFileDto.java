9700a037c199483e7b1d2a6927095e349826b008
qwelas <qwelas007@gmail.com>
2016-04-10 18:25:04 +0100

    Done PlainFileDto close #58

---
+++ src/main/java/pt/tecnico/myDrive/service/dto/PlainFileDto.java
@@ -0,0 +1,53 @@
+package pt.tecnico.myDrive.service.dto;
+
+import org.joda.time.DateTime;
+
+
+public class PlainFileDto extends FileDto {
+
+   
+    private String _content;
+
+    /**
+     * Construtor to PlainFileDto
+     * @param id (int) receives the id of the file.
+     * @param name (String) receives the name of the file.
+     * @param modified (DateTime) receives the date of the file.
+     * @param umask (Short) receives the permission mask of the file.
+     * @param parent (String) receives the name of the parent directory.
+     * @param owner (String) receives the owner name of the file.
+     * @param content (String) receives the owner name of the file.
+     */
+    public PlainFileDto(int id,
+                   String name,
+                   DateTime modified,
+                   short umask,
+                   String parent,
+                   String owner,
+                   String content) {
+        super(id, name, modified, umask, parent, owner);
+        _content = content;  
+    }
+
+    /**
+     * Getter for _content.
+     * @return int returns the content of the file.
+     */
+    public final String getContent() {
+        return _content;
+    }
+
+
+   
+    /**
+     * Override of operator equal to a specific one.
+     * @param o represents the other object.
+     * @return boolean returns 1 if the name is equal to the other.
+     */
+    
+    public int compareTo(PlainFileDto o) {
+        return getName().compareTo(o.getName());
+    }
+
+
+}
