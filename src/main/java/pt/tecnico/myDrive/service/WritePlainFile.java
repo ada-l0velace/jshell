a64272ab02f352dbae3874243c297a44b92b6796
qwelas <qwelas007@gmail.com>
2016-04-10 18:44:38 +0100

    Created the base for the WritePlainFile service #44

---
+++ src/main/java/pt/tecnico/myDrive/service/WritePlainFile.java
@@ -0,0 +1,16 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+
+public class WritePlainFile extends MyDriveService {
+
+    public WritePlainFile(String token, String plainfileName, String content) {
+
+    }
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+
+    }
+}
