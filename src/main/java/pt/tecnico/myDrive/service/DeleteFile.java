a8dfdf2f0b17718d7ba1c5122e8cd585db430bcd
Biana <yommere@gmail.com>
2016-04-11 16:27:17 +0100

    Added class skeleton #42

---
+++ src/main/java/pt/tecnico/myDrive/service/DeleteFile.java
@@ -0,0 +1,15 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class DeleteFile extends MyDriveService {
+
+    public DeleteFile(String token, String fileName) {
+
+    }
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+
+    }
+}
