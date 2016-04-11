2d21a9f18324c65211b0da4b578988dcb2937cbc
Biana <yommere@gmail.com>
2016-04-11 17:29:15 +0100

    Created DeletePermissionException #69

---
+++ src/main/java/pt/tecnico/myDrive/exception/DeletePermissionException.java
@@ -0,0 +1,22 @@
+package pt.tecnico.myDrive.exception;
+
+public class DeletePermissionException extends MyDriveException {
+
+    private static final long serialVersionUID = 1L;
+
+    private String _filename;
+    private String _username;
+
+    public DeletePermissionException(String filename, String username) {
+        _filename = filename;
+        _username = username;
+    }
+
+    public String getFilename() {return _filename;}
+    public String getUsername() {return _username;}
+
+    @Override
+    public String getMessage() {
+        return "The user: " + getUsername() + " can't doesn't have permission to delete " + getFilename();
+    }
+}
