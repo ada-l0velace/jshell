e6ebab82bbb4b24fbedb8fe691eb95815518de3f
Biana <yommere@gmail.com>
2016-04-11 17:43:29 +0100

    Added body to delete file service not finished yet #42

--- src/main/java/pt/tecnico/myDrive/service/DeleteFile.java
+++ src/main/java/pt/tecnico/myDrive/service/DeleteFile.java
@@ -1,15 +1,25 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.*;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
-public class DeleteFile extends MyDriveService {
+public class DeleteFile extends LoginRequiredService {
+
+    private User _user;
+    private Session _session;
+    private String _filename;
 
     public DeleteFile(String token, String fileName) {
-
+        super(token);
+        _user = Manager.getInstance().getUserByToken(token);
+        _session = Manager.getInstance().getSessionByToken(token);
+        _filename = fileName;
     }
 
     @Override
     protected void dispatch() throws MyDriveException {
-
+        super.dispatch();
+        Directory dir = _session.getCurrentDirectory();
+        dir.remove(_user, _filename);
     }
 }
