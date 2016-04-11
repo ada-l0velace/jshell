c39cd4eb47409b2d612bcef322702949b776a799
nucleardannyd <daniel.leitao@ist.utl.pt>
2016-04-11 23:47:41 +0100

    Read Permission Exception completed #68

---
+++ src/main/java/pt/tecnico/myDrive/exception/ReadPermissionException.java
@@ -0,0 +1,23 @@
+package pt.tecnico.myDrive.exception;
+
+public class ReadPermissionException extends MyDriveException {
+
+    private static final long serialVersionUID = 1L;
+
+    private String _name;
+    private String _user;
+
+    public ReadPermissionException(String name, String user) {
+        _name = name;
+        _user = user;
+    }
+
+    public String getFileName(){
+        return _name;
+    }
+
+    @Override
+    public String getMessage(){
+        return _user + " doesn't have permissions to read " + getFileName() + " file";
+    }
+}
