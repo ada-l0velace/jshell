f7e857e3d7f123be61927a28f0a6d4f146008ed5
chicorexia <ist177991@tecnico.ulisboa.pt>
2016-04-13 00:11:27 +0100

    commit just to pull

--- src/test/java/pt/tecnico/myDrive/service/ChangeDirectoryTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ChangeDirectoryTest.java
@@ -1,4 +1,4 @@
-/*package pt.tecnico.myDrive.service;
+package pt.tecnico.myDrive.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -8,40 +8,68 @@ import org.junit.Test;
 import pt.tecnico.myDrive.domain.Manager;
 import pt.tecnico.myDrive.domain.User;
 import pt.tecnico.myDrive.domain.Session;
-import pt.tecnico.myDrive.exception.InvalidUsernameException;
-import pt.tecnico.myDrive.exception.InvalidUserCredentialsException;
+import pt.tecnico.myDrive.exception.UserSessionExpiredException;
+import pt.tecnico.myDrive.exception.FileNotFoundException;
 
-public class ChangeDirectoryTest extends BaseServiceTest{
+public class ChangeDirectoryTest extends TokenVerificationTest{
 
     private static final String _dirTest = "changeTester";
-    private File _changedDir;
-    
+    private static final String _username = "Dovah";
+    private static final String _password = "Khin";
+    private User _user;
+    private String _token;
+    private Session s;
+    private static final String dirName [] = {
+            "games",
+            "steam",
+            "Fallout",
+            "Isaac",
+            "Binding",
+            "Dark",
+            "Souls",
+            "lol",
+            "Malamar"
+        };
+    private static final String paths [] = {
+            "/",
+            "/home",
+            ".",
+            "..",
+            "/home/Dovah",
+            "/home/",
+            "/home/Dovah/",
+            "/",
+            "//"
+        };
     protected void populate(){
-        //_user = createUser(_username, _password, "John",(short) 255);
+        _user = createUser(_username, _password, "DragonBorn",(short) 255);
+        _token = createSession(username);
+        
+        for (String username : dirName && String path : paths) {
+        	_dirTest = createDir(_user , dirName, paths, Manager.getInstance());
+        }
     }
 
     @Test
+    public void emptyChange() {
+    	
+    }
+    @Test
     public void success(){
-        /*LoginUser service = new LoginUser(_username, _password);
-        service.execute();
-
-        //check session was created
-        Session s = Manager.getInstance().getSessionByToken(service.result());
+    	ChangeDirectory FullIvt = ChangeDirectory(log.result() , IventPath);
+        FullIvt.execute();
         
-        assertNotNull("Session was not created", s);
-        assertEquals("Invalid username", _username, s.getUser().getUsername());
+        assertFalse("O ficheiro nao foi trocado", s.getCurrentDirectory().getName().equals(IventPath));
     }
 
-    @Test(expected = InvalidUserCredentialsException.class)
-    public void invalidPassword(){
-        LoginUser service = new LoginUser(_username, "general");
-        service.execute();
-    }
-        
-    @Test(expected = InvalidUserCredentialsException.class)
-    public void invalidUsername(){
-        LoginUser service = new LoginUser("miranda", _password);
-        service.execute();
+    @Test(expected = FileNotFoundException.class)
+    public void invalidFile(){
+        ChangeDirectory Boom = ChangeDirectory(log.result() , "/voidBorn");
+        FullIvt.execute();
     }
     
-}*/
+    @Override
+    public MyDriveService CreateService(String token) {
+        return new ListDirectory(token);
+    }
+}
