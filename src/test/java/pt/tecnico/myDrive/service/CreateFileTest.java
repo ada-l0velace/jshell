99626e5364bd6159d4c4157158ca24f3bf7a5052
qwelas <qwelas007@gmail.com>
2016-04-13 19:52:22 +0100

    Started CreateFileTest #48

---
+++ src/test/java/pt/tecnico/myDrive/service/CreateFileTest.java
@@ -0,0 +1,83 @@
+package pt.tecnico.myDrive.service;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import pt.tecnico.myDrive.domain.Manager;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.exception.FileNotFoundException;
+
+
+public class CreateFileTest extends TokenVerificationTest {
+	
+    private Directory _file;
+    private static final String FILETYPEL = "L";
+    private static final String FILETYPEP = "P";
+    private static final String FILETYPED = "D";
+    private static final String FILETYPEA = "A";
+	private static final String FILENAME = "TestFile";
+	private static final String USERNAME = "Jack";
+	private static final String PASSWORD = "pwjack";
+    private static final String CONTENT = "teste teste";
+    private static final String NAME = "John";
+    private static final Short UMASK = 0xF0;
+    private static final Short UMASK0 = 0x00;
+	private User _user;
+	private String _token;
+    private String _tokenNoAcess;
+	
+	
+    protected void populate() {
+		_user = createUser(USERNAME, PASSWORD, NAME, UMASK);
+		_token = createSession(USERNAME);
+
+        _user = createUser(USERNAME, PASSWORD, NAME, UMASK0);
+        _tokenNoAcess = createSession(USERNAME);
+    }
+
+    @Test
+    public void success() {
+        CreateFile service = new CreateFile(_token, FILENAME, FYLETPE);
+        service.execute();
+
+        // check file was created
+        assertFalse("File was not created", !_user.hasFile(_fileName));
+    }
+	
+    @Test(expected = FileNotFoundException.class)
+    public void deleteNonExistingFile() {
+        CreateFile service = new CreateFile(_token, "Bin");
+        service.execute();
+    }
+
+    @Test
+    public void createdPlainFile() {
+        CreateFile service = new CreateFile(_token, FILENAME, FYLETPP, CONTENT);
+        service.execute();
+
+        // check file was created
+        assertFalse("File was not created", !_user.hasFile(_fileName));
+    }
+
+    @Test(expected = ReadPermissionException.class)
+    public void creatNonAcesivelFile() {
+        ReadPlainFile service = new  CreateFile(_tokenNoAcess, FILENAME, FYLETPA);
+        service.execute();
+    }
+
+    @Test(expected = ReadPermissionException.class)
+    public void creatNonAcesivelPlainFile() {
+        ReadPlainFile service = new  CreateFile(_tokenNoAcess, FILENAME, FYLETPP, CONTENT);
+        service.execute();
+    }
+
+    
+    @Override
+    public MyDriveService CreateService(String token) {
+        return new CreateFile(token, _fileName);
+    }
+
+
+}
