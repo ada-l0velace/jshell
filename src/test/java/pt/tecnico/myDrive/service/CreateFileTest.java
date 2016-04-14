ee67704b2a098761dc4579d7586b6e9beca5712d
nucleardannyd <daniel.leitao@ist.utl.pt>
2016-04-14 01:37:36 +0100

    CreateFile success exceptions added #48

--- src/test/java/pt/tecnico/myDrive/service/CreateFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/CreateFileTest.java
@@ -9,22 +9,90 @@ import pt.tecnico.myDrive.domain.User;
 import pt.tecnico.myDrive.domain.Directory;
 import pt.tecnico.myDrive.exception.FileNotFoundException;
 import pt.tecnico.myDrive.exception.ReadPermissionException;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
 public class CreateFileTest extends TokenVerificationTest {
 	
     private Directory _dir;
+    private String _username = "rikku";
+    private String _filename = "heartless";
+    private String _content = "OrganizationXIII rules!!!";
+    private String _token;
+    private User _user;
+    private Manager m;
     
     protected void populate() {
+        _user = createUser(_username, "keyblademaster", "Sora", (short) 255);
+        _token = createSession(_username);
+        m = Manager.getInstance();
+        _dir = new Directory();
     }
 
     @Test
-    public void success() {
+    public void createAppWithoutContent(){
+        CreateFile service = new CreateFile(_token, _filename, "A");
+        service.execute();
     }
-    
+
+    @Test
+    public void createAppWithContent(){
+        CreateFile service = new CreateFile(_token, _filename, "A", _content);
+        service.execute();
+    }
+
+    @Test
+    public void createPlainfileWithoutContent(){
+        CreateFile service = new CreateFile(_token, _filename, "P");
+        service.execute();
+    }
+
+    @Test
+    public void createPlainfileWithContent(){
+        CreateFile service = new CreateFile(_token, _filename, "P", _content);
+        service.execute();
+    }
+
+    @Test
+    public void createDirectoryWithoutContent(){
+        CreateFile service = new CreateFile(_token, _filename, "D");
+        service.execute();
+    }
+
+    @Test
+    public void createDirectoryWithContent(){
+        CreateFile service = new CreateFile(_token, _filename, "D", _content);
+        service.execute();
+    }
+
+    @Test
+    public void createLinkWithPath() {
+        CreateFile service = new CreateFile(_token, _filename, "L", "/home");
+        service.execute();
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void createLinkWithPathWithoutFile() {
+        CreateFile service = new CreateFile(_token, _filename, "L", _content);
+        service.execute();
+    }
+
+    @Test
+    public void createLinkWithoutContent() {
+        CreateFile service = new CreateFile(_token, _filename, "L");
+        service.execute();
+    }
+
+
+    @Test
+    public void fileWithDifferentPermissionsThanUser() {
+        CreateFile service = new CreateFile(_token, _filename, "A", "myContent");
+        service.execute();
+        assertEquals(m.getUserByToken(_token).getPermissions(), m.getSessionByToken(_token).getCurrentDirectory().searchFile(_filename).getPermissions());
+    }
+                        
     @Override
     public MyDriveService CreateService(String token) {
-        return new CreateFile(token, "somethingfile", "l");
+        return new CreateFile(token, "somethingfile", "L");
     }
 
 
