dfa897a4f80ce1ba56bf2a56f91ca75ca529371f
qwelas <qwelas007@gmail.com>
2016-04-25 18:37:55 +0100

    Expanded the Coverage of the ReadPlainFileTest #98

--- src/test/java/pt/tecnico/myDrive/service/ReadPlainFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ReadPlainFileTest.java
@@ -18,10 +18,12 @@ public class ReadPlainFileTest extends TokenVerificationTest {
 
 	private PlainFile _file;
 	private static final String _fileName = "Testdoc";
+    private static final String _fileName1 = "Filesemperm";
 	private static final String _testContent = "vanilla";
 	private static final String _password = "pwjack";
     private static final String _name = "Stevie";
     private static final Short _umask = 0xF0;
+    private static final Short _umask0 = 0x70;
 
     private User _user;
 	private String _token;
@@ -29,6 +31,9 @@ public class ReadPlainFileTest extends TokenVerificationTest {
 
     private User _user1;
     private String _token1;
+
+    private User _user2;
+    private String _token2;
 
     private PlainFile _file2;
 
@@ -44,12 +49,16 @@ public class ReadPlainFileTest extends TokenVerificationTest {
         //Second user the read.
         _user1 = createUser("derp1", _password, _name, _umask);
         _token1 = createSession("derp1", _password);
+        //User without premissions
+        _user2 = createUser("derp2", _password, _name, _umask0);
+        _token2 = createSession("derp2", _password);
         //Root token.
         _rootToken = createSession("root", "***");
         //Change directory to check read permissions
         Session s = m.getSessionByToken(_token);
         new PlainFile(m.getSuperuser(), _fileName, _testContent , s.getCurrentDirectory(), m);
         new PlainFile(_user, _fileName + "1", _testContent , s.getCurrentDirectory(), m);
+        new PlainFile(_user, _fileName1, _testContent , s.getCurrentDirectory(), m);
         new Directory(_user, "DirToTheFuture", s.getCurrentDirectory(), m);
         _file = new App(m.getSuperuser(), "AppToThePast", _testContent , s.getCurrentDirectory(), m);
         _pathLink = _user.createLink(_file, "LinkToThePast", s.getCurrentDirectory(),m);
@@ -98,6 +107,12 @@ public class ReadPlainFileTest extends TokenVerificationTest {
     }
 
     @Test(expected = ReadPermissionException.class)
+    public void fileReadUserAccessDenied() {
+        ReadPlainFile service = new ReadPlainFile(_token2, _fileName1);
+        service.execute();
+    }
+
+    @Test(expected = ReadPermissionException.class)
     public void fileReadAccessDenied() {
         Manager m = Manager.getInstance();
         Session s = m.getSessionByToken(_token);
@@ -108,6 +123,7 @@ public class ReadPlainFileTest extends TokenVerificationTest {
         ReadPlainFile service = new ReadPlainFile(_token1, _fileName + "1");
         service.execute();
     }
+
 
     @Override
     public MyDriveService CreateService(String token) {
