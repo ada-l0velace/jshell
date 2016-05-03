e028b253337d383d81310f022520453c81ae8d36
Biana <yommere@gmail.com>
2016-05-03 11:54:36 +0100

    Added skeleton to Execute File Service Tests #105

---
+++ src/test/java/pt/tecnico/myDrive/service/ExecuteFileServiceTest.java
@@ -0,0 +1,18 @@
+package pt.tecnico.myDrive.service;
+
+import org.junit.Test;
+
+public class ExecuteFileServiceTest extends TokenVerificationTest 
+{
+      
+    protected void populate() 
+    {
+        
+    }
+
+    @Test
+    public void success()
+    {
+    
+    }
+}
