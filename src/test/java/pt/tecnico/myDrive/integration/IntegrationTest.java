ef64d7a1ffb4cde5f1e72c56dcdf8858580791e7
inestormenta <ist177980@tecnico.ulisboa.pt>
2016-05-05 23:45:42 +0100

    Added some integration tests #92

--- src/test/java/pt/tecnico/myDrive/integration/IntegrationTest.java
+++ src/test/java/pt/tecnico/myDrive/integration/IntegrationTest.java
@@ -18,18 +18,45 @@ import org.jdom2.output.Format;
 import pt.tecnico.myDrive.service.*;
 import pt.tecnico.myDrive.service.dto.*;
 import pt.tecnico.myDrive.exception.*;
+import pt.tecnico.myDrive.service.factory.Factory.FileType;
+
 
 //@RunWith(JMockit.class)
 public class IntegrationTest extends BaseServiceTest {
 
-    
-    protected void populate() { // populate mockup
+	private static final String _username = "toni", _pass = "tonitoni16";
+	private static final String _nameDir1 = "Documents",  _nameDir2= "Games", _nameApp = "App";
+
 	
+	protected void populate() { // populate mockup
+		
     }
 
+	
     @Test
     public void success() throws Exception {
 
+    	LoginUserService login = new LoginUserService(_username, _pass);
+        login.execute();
+        String token = login.result();
         
+        new CreateFileService(token, _nameDir1, FileType.DIRECTORY).execute();
+        new CreateFileService(token, _nameDir2, FileType.DIRECTORY).execute();
+        new CreateFileService(token, _nameApp, FileType.APP, "").execute();
+
+        ListDirectoryService ld = new ListDirectoryService(token);
+    	ld.execute();
+    	assertEquals(ld.result().size(), 3); // Documents, Images, App
+    	
+    	new DeleteFileService(token, _nameDir2).execute();
+    	ld = new ListDirectoryService(token);
+    	ld.execute();
+    	assertEquals(ld.result().size(), 2); //Documents, App
+    	
+    	new WritePlainFileService(token, _nameApp, "newContent").execute();
+    	
+    	ReadPlainFileService rpf = new ReadPlainFileService(token, _nameApp);
+    	rpf.execute();    	
+        assertEquals(rpf.result(), "newContent");
     }
 }
