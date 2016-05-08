6b887fed717055c183ae17bc155342d213363a60
chicorexia <ist177991@tecnico.ulisboa.pt>
2016-05-08 16:47:56 +0100

    Added more tests to AddEnvironmentServiceTests #104

--- src/main/java/pt/tecnico/myDrive/service/dto/EnvironmentVariableDto.java
+++ src/main/java/pt/tecnico/myDrive/service/dto/EnvironmentVariableDto.java
@@ -4,7 +4,15 @@ package pt.tecnico.myDrive.service.dto;
  * Created by lolstorm on 26/04/16.
  */
 public class EnvironmentVariableDto {
+	
+	public EnvironmentVariableDto(String name, String Value){
+		
+	}
 	public String getName(){
+		return "";
+	}
+	
+	public String getValue(){
 		return "";
 	}
 }
--- src/test/java/pt/tecnico/myDrive/service/AddEnvironmentVariableTest.java
+++ src/test/java/pt/tecnico/myDrive/service/AddEnvironmentVariableTest.java
@@ -39,6 +39,7 @@ public class AddEnvironmentVariableTest extends TokenVerificationTest{
         s = m.getSessionByToken(_token);
         _rootToken = createSession("root", "***");
         rootSession = m.getSessionByToken(_rootToken);
+        EnvironmentVariableDto newVariavel = new EnvironmentVariableDto("$cenas", "coisas");
     }
 
     @Test
@@ -68,109 +69,40 @@ public class AddEnvironmentVariableTest extends TokenVerificationTest{
     	}
     	assertTrue("nao adicionou corretamente a variavel com o root", varCheck);
     }
+    
+    @Test
+    public void reDefineSuccess() {
+    	boolean varCheck = false;
+    	EnvironmentVariableService EVS = new EnvironmentVariableService(_rootToken, "$cenas", "banana");
+    	EVS.execute();
+    	List<EnvironmentVariableDto> aev = EVS.result();
+    	for(int i = 0; i < aev.size();i ++){
+    		if(aev.get(i).getName().equals("$cenas") && aev.get(i).getValue().equals("banana")){
+    			varCheck = true;
+    		}
+    	}
+    	assertTrue("nao redefiniu bem a variavel", varCheck);
+    }
 
     @Test(expected = InvalidVariableNameException.class)
     public void invalidName() {
     	EnvironmentVariableService EVS = new EnvironmentVariableService(_token, "", "banana");
     	EVS.execute();
     }
+    
     @Test(expected = EmptyVariableValueException.class)
     public void noContent() {
     	EnvironmentVariableService EVS = new EnvironmentVariableService(_token, "$urso", "");
     	EVS.execute();
     }
     
-    /*@Test
-    public void failedPartialPath() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_token, "games/lol");
-    	FullIvt.execute();
-    	s = m.getSessionByToken(FullIvt.result());
-    	assertTrue("nao mudou corretamente de diretorio", s.getCurrentDirectory().getName().equals("lol"));
-    }
-
-    @Test
-    public void rootNoChange() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_rootToken, "/home/Dovah/Fallout");
-    	FullIvt.execute();
-    	rootSession = m.getSessionByToken(FullIvt.result());
-    	assertTrue("root sem permissoes?", rootSession.getCurrentDirectory().getName().equals("Fallout"));
-    }
-
-    @Test
-    public void couldNotChangeRootDir() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_token, "/Dark");
-    	FullIvt.execute();
-    	s = m.getSessionByToken(FullIvt.result());
-    	assertTrue("user nao conseguiu alterar um diretorio do root", s.getCurrentDirectory().getName().equals("Dark"));
-    }
-
-    @Test
-    public void dotPathFail() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_rootToken, ".");
-    	String prechange = rootSession.getCurrentDirectory().getName();
-    	FullIvt.execute();
-    	rootSession = m.getSessionByToken(FullIvt.result());
-    	assertTrue("nao mudou corretamente para ele proprio", prechange.equals(rootSession.getCurrentDirectory().getName()));
-    }
-
-    
-    @Test
-    public void dotDotPathFail() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_rootToken, "..");
-    	String prechange = rootSession.getCurrentDirectory().getParent().getName();
-    	FullIvt.execute();
-    	rootSession = m.getSessionByToken(FullIvt.result());
-    	assertTrue("nao mudou corretamente para o pai", rootSession.getCurrentDirectory().getName().equals(prechange));
-    }
-
-    @Test(expected = FileNotFoundException.class)
-    public void invalidFile(){
-        ChangeDirectoryService Boom = new ChangeDirectoryService(_rootToken , "/voidBorn");
-        Boom.execute();
-    }
-
-    @Test(expected = FileNotFoundException.class)
-    public void noPath(){
-        ChangeDirectoryService Boom = new ChangeDirectoryService(_rootToken , "");
-        Boom.execute();
+    @Test(expected = EmptyVariableValueException.class)
+    public void noContentRedefine() {
+    	EnvironmentVariableService EVS = new EnvironmentVariableService(_token, "$cenas", "");
+    	EVS.execute();
     }
     
-    @Test(expected = FileNotFoundException.class)
-    public void invalidPartialPath(){
-        ChangeDirectoryService Boom = new ChangeDirectoryService(_rootToken , "balelas/outrasbalelas");
-        Boom.execute();
-    }
-    
-    @Test(expected = InvalidNameFileException.class)
-    public void tooMuchChars() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_rootToken, "/home/" + giantName);
-    	FullIvt.execute();
-    }
-    
-    @Test(expected = InvalidFileTypeException.class)
-    public void wrongFileType() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_rootToken, "/home/Dovah/oneAboveAll");
-    	FullIvt.execute();
-    }
-    
-    @Test(expected = InvalidFileTypeException.class)
-    public void wrongAppType() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_rootToken, "/home/Dovah/appBroker");
-    	FullIvt.execute();
-    }
-    
-    @Test(expected = InvalidFileTypeException.class)
-    public void wrongLinkType() {
-    	ChangeDirectoryService FullIvt = new ChangeDirectoryService(_rootToken, "/home/Dovah/aLinkToTheCrash");
-    	FullIvt.execute();
-    }
 
-    @Test(expected = ReadPermissionException.class)
-    public void surpassedPermission(){
-        ChangeDirectoryService Boom = new ChangeDirectoryService(_token , "/home/Nathan/Binding");
-        Boom.execute();
-    }
-*/
     @Override
     public MyDriveService CreateService(String token) {
         return new ChangeDirectoryService(token, "/");
