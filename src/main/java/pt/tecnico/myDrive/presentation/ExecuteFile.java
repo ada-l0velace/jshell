930acd91d6571327a5d860c546e290364282e9c0
chicorexia <ist177991@tecnico.ulisboa.pt>
2016-05-08 19:04:40 +0100

    Added almost all functionalities to Execute command #85

--- src/main/java/pt/tecnico/myDrive/presentation/ExecuteFile.java
+++ src/main/java/pt/tecnico/myDrive/presentation/ExecuteFile.java
@@ -2,16 +2,27 @@ package pt.tecnico.myDrive.presentation;
 
 import pt.tecnico.myDrive.service.ExecuteFileService;
 
+import java.util.Arrays;
 
 public class ExecuteFile extends MyDriveCommand {
-
+	
+	public static String token;
+	
     public ExecuteFile(Shell sh) { super(sh, "do", "This command executes a file with the args."); }
+    
     public void execute(String[] args) {
         if (args.length < 1)
             throw new RuntimeException("USAGE: "+name()+" <path> [<args>]");
-        if (args.length > 1)
+        else if (args.length > 1)
+        {
+        	String[] newArray = Arrays.copyOfRange(args, 1, args.length);
+        	new ExecuteFileService(token, args[0], newArray);
             System.out.println("FIXME");
+        }
         else
+        {
+        	new ExecuteFileService(token, args[0]);
             System.out.println("FIXME");
+        }
     }
 }
