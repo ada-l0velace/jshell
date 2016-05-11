82a1e43cadcd2e49a8269bd05c7a041a2dd58a41
qwelas <qwelas007@gmail.com>
2016-05-11 16:32:59 +0100

    Completed the EnvironmentVariabledto #106

--- src/main/java/pt/tecnico/myDrive/service/dto/EnvironmentVariableDto.java
+++ src/main/java/pt/tecnico/myDrive/service/dto/EnvironmentVariableDto.java
@@ -4,15 +4,23 @@ package pt.tecnico.myDrive.service.dto;
  * Created by lolstorm on 26/04/16.
  */
 public class EnvironmentVariableDto {
+
+	private String _name;
+	private String _value;
 	
-	public EnvironmentVariableDto(String name, String Value){
+	public EnvironmentVariableDto(String name, String value){
 		
+		_name = name;
+		_value = value;
+			
 	}
 	public String getName(){
-		return "";
+		return _name;
 	}
 	
 	public String getValue(){
-		return "";
+		return  _value;
+
 	}
+	
 }
