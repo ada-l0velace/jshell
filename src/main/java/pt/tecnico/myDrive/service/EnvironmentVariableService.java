ffa9a7bd1a1232511a17e2299d0b23a77453ec68
Biana <yommere@gmail.com>
2016-05-11 18:43:01 +0100

    Started implementing service #79

--- src/main/java/pt/tecnico/myDrive/service/EnvironmentVariableService.java
+++ src/main/java/pt/tecnico/myDrive/service/EnvironmentVariableService.java
@@ -3,17 +3,29 @@ package pt.tecnico.myDrive.service;
 import pt.tecnico.myDrive.exception.MyDriveException;
 import pt.tecnico.myDrive.service.dto.EnvironmentVariableDto;
 
-import java.util.List;
+import java.util.List; 
+
+private User _user;
+private Session _session;
+private String _token;
+private String _name;
+private String _value;
 
 
 public class EnvironmentVariableService extends LoginRequiredService {
     public EnvironmentVariableService(String token, String name, String value) {
         super(token);
+        _token = token;
+        _name = name;
+        _value = value;
+        _user = Manager.getInstance().getUserByToken(token);
+        _session = Manager.getInstance().getSessionByToken(token);
     }
 
     @Override
     protected void dispatch() throws MyDriveException {
         super.dispatch();
+        
     }
 
     public List<EnvironmentVariableDto> result() {
