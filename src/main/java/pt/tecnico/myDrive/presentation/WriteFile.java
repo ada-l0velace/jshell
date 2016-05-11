fb967e4da01a1f088ac5107b7661fb5e7c1edab9
qwelas <qwelas007@gmail.com>
2016-05-11 17:13:59 +0100

    Made WriteFile Command #86

--- src/main/java/pt/tecnico/myDrive/presentation/WriteFile.java
+++ src/main/java/pt/tecnico/myDrive/presentation/WriteFile.java
@@ -2,16 +2,20 @@ package pt.tecnico.myDrive.presentation;
 
 import pt.tecnico.myDrive.service.WritePlainFileService;
 
-
 public class WriteFile extends MyDriveCommand {
 
+
     public WriteFile(Shell sh) { super(sh, "update", "This command updates a file content."); }
+
     public void execute(String[] args) {
-        if (args.length < 2)
-            throw new RuntimeException("USAGE: "+name()+" <path> <text>");
-        if (args.length > 2)
-            System.out.println("FIXME");
+
+        if (args.length == 2){
+            String path = args[0];
+            String content = args[1];
+            new WritePlainFileService(Token.token, path, content);
+        }
+
         else
-            System.out.println("FIXME");
+            throw new RuntimeException("USAGE: " + name() + " <path> <text>");
     }
 }
