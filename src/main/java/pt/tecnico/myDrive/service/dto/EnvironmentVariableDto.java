e4b02256c3a533a8f330fcc879033c41cc08e6aa
qwelas <qwelas007@gmail.com>
2016-05-11 16:36:42 +0100

    Added the javadoc for the EnvironmentVariableDto #106

--- src/main/java/pt/tecnico/myDrive/service/dto/EnvironmentVariableDto.java
+++ src/main/java/pt/tecnico/myDrive/service/dto/EnvironmentVariableDto.java
@@ -7,6 +7,12 @@ public class EnvironmentVariableDto {
 
 	private String _name;
 	private String _value;
+
+	/**
+     * Construtor to EnvironmentVariableDto
+     * @param name (String) receives the name of the EnvironmentVariable.
+     * @param value (String) receives the value of the EnvironmentVariable.
+     */
 	
 	public EnvironmentVariableDto(String name, String value){
 		
@@ -14,10 +20,21 @@ public class EnvironmentVariableDto {
 		_value = value;
 			
 	}
+
+	/**
+     * Getter for _name.
+     * @return String returns the name of the EnvironmentVariable.
+     */
+
 	public String getName(){
 		return _name;
 	}
 	
+	/**
+     * Getter for _value.
+     * @return String returns the value of the EnvironmentVariable.
+     */
+
 	public String getValue(){
 		return  _value;
 
